02eb2757514f72d22640f149a7277857a6fc579a
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-03-18 10:54:09 +0900

    feat: 내부 API POST 요청 관련 처리

--- pages/api/openAI.js
+++ pages/api/openAI.js
@@ -3,40 +3,39 @@
 import axios from "axios";
 
 export default async (req, res) => {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
   const { messageArr, pUid } = req.body;
   const api_url = `${process.env.NEXT_PUBLIC_URL}/openAI/consulting_emotion_pupu`;
   let parseMessageArr;
 
-  if (req.method === "POST") {
-    try {
-      if (typeof messageArr === "string") {
-        parseMessageArr = JSON.parse(messageArr);
-      } else parseMessageArr = [...messageArr];
+  try {
+    if (typeof messageArr === "string") {
+      parseMessageArr = JSON.parse(messageArr);
+    } else parseMessageArr = [...messageArr];
 
-      const response = await axios
-        .post(
-          api_url,
-          {
-            EBTData: { messageArr, pUid },
+    const response = await axios
+      .post(
+        api_url,
+        {
+          EBTData: { messageArr, pUid },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
           },
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then((res) => res.data);
-      // console.log(response);
-      res.json(response);
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: "Next.js 서버리스(openAI)에서 오류가 발생했습니다." });
-    }
-  } else {
-    // POST 이외의 요청에 대해 405 Method Not Allowed 반환
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+        }
+      )
+      .then((res) => res.data);
+    // console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Next.js 서버리스(openAI)에서 오류가 발생했습니다." });
   }
 };
