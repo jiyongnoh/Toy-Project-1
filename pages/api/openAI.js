14650cd70982af98c9f8f4f7d89d1c7e6f2c1349
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-03-18 10:37:15 +0900

    feat: 내부 api POST 요청 관련 처리 추가

--- pages/api/openAI.js
+++ pages/api/openAI.js
@@ -7,30 +7,36 @@ export default async (req, res) => {
   const api_url = `${process.env.NEXT_PUBLIC_URL}/openAI/consulting_emotion_pupu`;
   let parseMessageArr;
 
-  try {
-    if (typeof messageArr === "string") {
-      parseMessageArr = JSON.parse(messageArr);
-    } else parseMessageArr = [...messageArr];
+  if (req.method === "POST") {
+    try {
+      if (typeof messageArr === "string") {
+        parseMessageArr = JSON.parse(messageArr);
+      } else parseMessageArr = [...messageArr];
 
-    const response = await axios
-      .post(
-        api_url,
-        {
-          EBTData: { messageArr, pUid },
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
+      const response = await axios
+        .post(
+          api_url,
+          {
+            EBTData: { messageArr, pUid },
           },
-        }
-      )
-      .then((res) => res.data);
-    // console.log(response);
-    res.json(response);
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ message: "Next.js 서버리스(openAI)에서 오류가 발생했습니다." });
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => res.data);
+      // console.log(response);
+      res.json(response);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "Next.js 서버리스(openAI)에서 오류가 발생했습니다." });
+    }
+  } else {
+    // POST 이외의 요청에 대해 405 Method Not Allowed 반환
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
