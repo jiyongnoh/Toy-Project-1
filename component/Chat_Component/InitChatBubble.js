3314b840eddb65d860b59882766f1c1fdd3ee0fa
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-06-11 10:08:38 +0900

    feat: 엘라 상담 시작 관련 버튼 디자인 수정 + 버튼 중복 클릭 버그 수정

--- component/Chat_Component/InitChatBubble.js
+++ component/Chat_Component/InitChatBubble.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-import AudioPlayerButton from './AudioPlayerButton';
-import VideoModal from './VideoModal';
+// import AudioPlayerButton from './AudioPlayerButton';
+// import VideoModal from './VideoModal';
 
-const InitChatBubble = ({ message, isMine, role, btn, setTestType }) => {
+const ebtClassMapKorean = {
+  School: '학업/성적',
+  Friend: '대인관계',
+  Family: '가족관계',
+  Mood: '기분',
+  Health: '신체증상',
+  Self: '자기이해',
+};
+
+const InitChatBubble = ({
+  message,
+  isMine,
+  role,
+  btn,
+  setTestType,
+  testType,
+}) => {
   // console.log(media);
   return (
     <>
       {btn ? (
-        <AudioContanier message={message}>
-          <StyledBubble isMine={isMine} role={role}>
-            <MessageP>
-              <button
-                value={message}
-                onClick={(e) => {
-                  setTestType(e.target.value);
-                }}
-              >
-                {message}
-              </button>
-            </MessageP>
+        <InitContanier message={message}>
+          <StyledBubble isMine={isMine} role={role} btn={btn}>
+            <InitButton
+              value={message}
+              onClick={(e) => {
+                if (!testType) setTestType(e.target.value);
+              }}
+            >
+              {ebtClassMapKorean[message]}
+            </InitButton>
           </StyledBubble>
-        </AudioContanier>
+        </InitContanier>
       ) : (
         <StyledBubble isMine={isMine} role={role}>
-          <p>{message}</p>
+          <MessageP>{message}</MessageP>
         </StyledBubble>
       )}
     </>
@@ -34,7 +48,7 @@ const InitChatBubble = ({ message, isMine, role, btn, setTestType }) => {
 // Styled Components
 const StyledBubble = styled.div`
   max-width: 100%;
-  padding: 10px;
+  padding: ${(props) => (props.btn ? '0px' : '10px')};
   border-radius: 10px;
   margin: 0.2rem 0.1rem;
   word-wrap: break-word;
@@ -52,13 +66,7 @@ const StyledBubble = styled.div`
   align-items: center;
 `;
 
-const MessageP = styled.p`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
-const AudioContanier = styled.div`
+const InitContanier = styled.div`
   display: flex;
   flex-direction: ${(props) => (props.message.length < 30 ? 'row' : 'column')};
   justify-content: ${(props) =>
@@ -67,16 +75,33 @@ const AudioContanier = styled.div`
     props.message.length < 30 ? 'center' : 'flex-start'};
 `;
 
-const MediaButton = styled.button`
-  font-size: 1rem;
-  max-width: 100%;
-  padding: 10px;
-  border: 1px solid gray;
+const MessageP = styled.p`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const InitButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 15px;
+  border: none;
   border-radius: 10px;
-  margin: 0.2rem 0.1rem;
-  color: white;
   background-color: #a374db;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #8a5bbf; /* hover 색상 변경 */
+  }
+
+  &:active {
+    background-color: #096bb6; /* active 색상도 비슷하게 변경 */
+  }
 `;
 
 export default InitChatBubble;
