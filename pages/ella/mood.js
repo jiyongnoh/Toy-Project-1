bd5468c525f58939f75710cb18bcedf5ce691854
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-07-12 16:06:04 +0900

    feat: 엘라 기분관리 프로그램 페이지 수정. 인지행동 추가

--- pages/ella/mood.js
+++ pages/ella/mood.js
@@ -6,6 +6,8 @@ import { useEffect, useState, useRef } from 'react';
 import { handleTrainingMoodElla } from '@/fetchAPI/ellaTrainingAPI';
 
 import FixBubble from '@/component/EllaTraning_Component/FixBubble';
+import SelectBubble from '@/component/EllaTraning_Component/SelectBubble';
+
 import Image from 'next/image';
 import LoadingAnimation from '@/component/Chat_Component/LoadingAnimation';
 import { useRouter } from 'next/router';
@@ -20,7 +22,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export default function Test() {
   const [isPending, setIsPending] = useState(false);
   const [next, setNext] = useState(false); // 유저 문항 선택 트리거
-  const [select, setSelect] = useState('2'); // 유저 문항 선택지 1 || 2
   const [bottom, setBottom] = useState(false); // scrollToBottom 메서드 발동 트리거
   const [resultTrigger, setResultTrigger] = useState(false); // 결과 분석 요청 선택 트리거
   const [messageArr, setMessageArr] = useState([]);
@@ -34,6 +35,7 @@ export default function Test() {
   const [inputTrigger, setInputTrigger] = useState(false); // 제너레이터 반환값이 input인 경우 발동될 트리거
   const [gptTrigger, setGptTrigger] = useState(false); // 제너레이터 반환값이 gpt인 경우 발동될 트리거
   const [selectTrigger, setSelectTrigger] = useState(false); // 제너레이터 반환값이 select인 경우 발동될 트리거
+  const [select, setSelect] = useState(-1); // 유저 문항 선택지 1 || 2
 
   const router = useRouter();
   // 제너레이터는 리렌더링 시점에 초기화 => useRef를 통해 인스턴스 고정
@@ -59,10 +61,19 @@ export default function Test() {
   const createGptText = async (gptData) => {
     const { code, gpt_input } = gptData;
     try {
-      // 감정 분석 API 호출 이후 state 갱신
+      const tmp = messageArr.map((el) => {
+        let role = el.role;
+        let textArr = el.fix_content
+          .filter((el) => el.key === 'text' && el.value)
+          .map((el) => {
+            return { role, content: el.value };
+          });
+        return textArr[0];
+      });
+      // 엘라 API 호출 이후 state 갱신
       const data = await handleTrainingMoodElla({
         pUid: localStorage.getItem('id'),
-        messageArr,
+        messageArr: [...tmp.filter((el) => el)],
         code,
         ...gpt_input,
       });
@@ -115,35 +126,19 @@ export default function Test() {
       // 검사 문항 진행
       if (!done) {
         setNext(false);
-        if (value.type === 'fix') {
-          setGeneratorData({ ...value });
-          setFixTrigger(true);
-        }
-        if (value.type === 'input') {
-          setGeneratorData({ ...value });
-          setInputTrigger(true);
-        }
-        if (value.type === 'gpt') {
-          setGeneratorData({ ...value });
-          setGptTrigger(true);
-        }
+        setGeneratorData({ ...value });
+        if (value.type === 'fix') setFixTrigger(true);
+        if (value.type === 'input') setInputTrigger(true);
+        if (value.type === 'gpt') setGptTrigger(true);
+        if (value.type === 'select') setSelectTrigger(true);
       }
       // 검사 문항 종료 - 결과 및 AI 분석 요청
       else if (value) {
-        // const { result, type } = value;
-        // setIsPending(true);
-        // setTimeout(() => {
-        //   setMessageArr([
-        //     ...messageArr,
-        //     {
-        //       role: 'assistant',
-        //       content: result,
-        //     },
-        //   ]);
-        //   setNext(false);
-        //   setResultTrigger(true);
-        //   setBottom(true);
-        // }, 1500);
+        const { sava_data } = value;
+        console.log(sava_data);
+        delete value.sava_data;
+        setMessageArr([...messageArr, value]);
+        // Todo. 회기별 DB 저장 API 호출
       } else return;
 
       setBottom(true);
@@ -181,6 +176,7 @@ export default function Test() {
       setInputTrigger(false);
       setFlagEnter(false);
       setTimeout(() => {
+        setIsPending(false);
         setNext(true);
       }, 1000);
     }
@@ -194,6 +190,14 @@ export default function Test() {
       createGptText(generatorData);
     }
   }, [gptTrigger]);
+
+  useEffect(() => {
+    if (selectTrigger) {
+      setMessageArr([...messageArr, generatorData]);
+      setSelectTrigger(false);
+    }
+    setBottom(true);
+  }, [selectTrigger]);
 
   // 스크롤 바텀
   useEffect(() => {
@@ -213,17 +217,20 @@ export default function Test() {
         height="100%"
         padding="0 1rem"
       >
-        {/* <Image
-          src="/src/soyesKids_Logo.png"
-          alt={"soyes_logo"}
-          width={529}
-          height={93}
-        /> */}
         <PTBox ref={chatBoxBody}>
           {/* <PTBoxHeader>성격 검사</PTBoxHeader> */}
           <PTBoxBody>
             {messageArr.map((el, index) => {
               if (el.type === 'fix') return <FixBubble fix_data={el} />;
+              else if (el.type === 'select')
+                return (
+                  <SelectBubble
+                    select_data={el}
+                    setChat={index === messageArr.length - 1 && setChat}
+                    setNext={index === messageArr.length - 1 && setNext}
+                    setSelect={index === messageArr.length - 1 && setSelect}
+                  />
+                );
               else return;
             })}
             {/* 로딩바 */}
@@ -237,7 +244,7 @@ export default function Test() {
               }}
               onKeyPress={(e) => {
                 if (e.key === 'Enter' && chat !== '' && !isPending) {
-                  console.log(chat);
+                  setIsPending(true);
                   setFlagEnter(true);
                 }
               }}
@@ -247,7 +254,7 @@ export default function Test() {
             <ChatBoxFooterButton
               onClick={() => {
                 if (chat !== '' && !isPending) {
-                  console.log(chat);
+                  setIsPending(true);
                   setFlagEnter(true);
                 }
               }}
@@ -259,16 +266,6 @@ export default function Test() {
                 width={72}
                 height={57}
               />
-              {/* {isPending || isInitPending ? (
-                <span class="material-symbols-outlined">block</span>
-              ) : (
-                <Image
-                  src="/src/Consult_IMG/Icon/Consult_Send_Icon_IMG.png"
-                  alt={'send_icon'}
-                  width={72}
-                  height={57}
-                />
-              )} */}
             </ChatBoxFooterButton>
           </ChatBoxFooter>
         </PTBox>
@@ -391,6 +388,7 @@ const ChatBoxFooter = styled.div`
 `;
 
 const ChatBoxFooterInput = styled.input`
+  display: ${(props) => (props.inputTrigger ? 'flex' : 'none')};
   width: 100%;
   padding: 2rem;
   border: 1px solid #e6e6e6;
@@ -424,6 +422,7 @@ const ChatBoxFooterInput = styled.input`
 `;
 
 const ChatBoxFooterButton = styled.button`
+  display: ${(props) => (props.inputTrigger ? 'flex' : 'none')};
   background: inherit;
   margin-left: 8px;
   padding: 5px 12px;
@@ -441,7 +440,6 @@ const ChatBoxFooterButton = styled.button`
   &:active {
     background-color: ${(props) => (props.inputTrigger ? '#B88CD5' : null)};
   }
-  display: flex;
   transition: 0.2s;
 `;
 
--- store/ellaGenerator.js
+++ store/ellaGenerator.js
@@ -1,6 +1,7 @@
 // 엘라 기분관리 훈련 프로그램
 // 1회기
 export function* ellaMood_Round_first() {
+  let answerArr = [];
   // 인사말
   yield {
     role: 'assistant',
@@ -181,5 +182,187 @@ export function* ellaMood_Round_first() {
     ],
   };
 
-  return { result: '종료' };
+  // 인지행동 치료 시작
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 1`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+
+  const answer1 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer1));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 2`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer2 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer2));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 3`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer3 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer3));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 4`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer4 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer4));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 5`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer5 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer5));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 6`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer6 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+  answerArr.push(parseInt(answer6));
+
+  yield {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value: `인지행동 문항 7`,
+      },
+      { key: 'img', value: '/src/PT_IMG/Test/PT_Question_IMG_1.png' }, // 인지행동 문항 이미지
+    ],
+  };
+  const answer7 = yield {
+    role: 'user',
+    type: 'select',
+    select_content: [
+      { selection: '선택지1', value: 0 },
+      { selection: '선택지2', value: 1 },
+      { selection: '선택지3', value: 2 },
+      { selection: '선택지4', value: 3 },
+    ],
+  };
+
+  answerArr.push(parseInt(answer7));
+
+  const congnitive_score = answerArr.reduce((acc, cur) => acc + cur);
+
+  return {
+    role: 'assistant',
+    type: 'fix',
+    fix_content: [
+      {
+        key: 'text',
+        value:
+          congnitive_score >= 18
+            ? '좋았어! 멋진 생각을 나눠준 너에게 씨앗을 줄게. 마음을 가꾸듯 잘 키워봐. 다음에 또 만나'
+            : '수고했어. 좀 더 분발해서 다음엔 씨앗을 받아보자. 다음 시간에 만나',
+      },
+    ],
+    sava_data: {
+      mood_name,
+      congnitive_score,
+    },
+  };
 }
